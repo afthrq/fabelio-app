035da8c1a8fde246efa2de36a8d748d96dc05d48
Ahmad Fathoriq <ahmad.fathoriq@gmail.com>
2019-10-30 18:19:13 +0700

    change database to use dynamic url

--- fabelio_app/settings.py
+++ fabelio_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,17 +75,9 @@ WSGI_APPLICATION = 'fabelio_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fabelio',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+db_from_env = dj_database_url.config()
+DATABASES = {}
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
